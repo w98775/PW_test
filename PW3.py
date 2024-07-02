6712a72f92783b9fd3a7b446be1e89439d57d040
Odelia chiu <odelia19941206@gmail.com>
2024-07-02 23:57:43 +0800

    three

---
+++ PW3.py
@@ -0,0 +1,20 @@
+# 密碼重試程式
+# password = 'a123456' 
+# 讓使用者重複輸入密碼
+# 最多輸入3次
+# 如果正確就印出登入成功，不正確就印出密碼錯誤，還有＿＿次幾會！
+
+password = 'a123456'
+i = 3 # 剩餘機會
+while i > 0:
+	i = i - 1
+	pwd = input('請輸入密碼：')
+	if pwd == password:
+		print('登入成功')
+		break # 逃出迴圈
+	else:
+		print('密碼錯誤！')
+		if i > 0:
+			print('還有' , i, '次機會')
+		else:
+			print('沒有機會了，要鎖帳號了！')
